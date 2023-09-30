edacdb09a344b66e10a6ff5a1e3a808f3bf97b0a
Busko Ilya <trassae95st@mail.ru>
2023-09-30 12:48:00 +0300

    Create bubble sort

--- Solution/SmartSort/bubble_sort.cpp
+++ Solution/SmartSort/bubble_sort.cpp
@@ -1,5 +1,14 @@
 #include "bubble_sort.hpp"
 
 void BubbleSort::sort(std::vector<int>& arr) {
-
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int x = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = x;
+            }
+        }
+    }
 }
--- Solution/SmartSort/main.cpp
+++ Solution/SmartSort/main.cpp
@@ -5,7 +5,6 @@
 int main() {
     std::vector<int> arr = { 8, 2, 5 };
 
-    // TODO: sorting
     BubbleSort::sort(arr);
 
     for (int i = 0; i < arr.size(); i++) {
