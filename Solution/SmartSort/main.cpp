6c77f174204fdc97530eb5b8a9c6b20b16da8a80
islanditurup <sandy1bridge@gmail.com>
2023-10-30 03:12:02 +0300

    [akrylov] Add my quick_sort, modified menu,fixed typo

--- Solution/SmartSort/main.cpp
+++ Solution/SmartSort/main.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::setlocale(LC_ALL, "Russian");
 
-    int arr[] = { 8, 2, 5,6,7,8,9,11,1,2,3,4,5 };
+    int arr[] = { 8, 2, 5};
     const int arr_size = sizeof(arr) / sizeof(arr[0]);
 
     Menu::Item isort_by_ibusko_bubble = {
