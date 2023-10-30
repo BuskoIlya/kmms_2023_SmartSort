e578bc4395f51f887c66fb4efd38b648ada2dfff
islanditurup <sandy1bridge@gmail.com>
2023-10-30 03:05:24 +0300

    [akrylov] Add my quick_sort, modified menu,fixed typo

--- Solution/SmartSort/menu.cpp
+++ Solution/SmartSort/menu.cpp
@@ -7,10 +7,12 @@
 #include "./sortings/asulimov_sortings.hpp"
 #include "./sortings/ibusko_sortings.hpp"
 
-void Menu::sort_by_akrylov_bubble(const char* const comment, int arr[], const int arr_size) {
+void Menu::sort_by_akrylov_quick(const char* const comment, int arr[], const int arr_size) {
     std::cout << comment << std::endl;
     IOArray::print_array("Первоначальный массив: ", arr, arr_size);
-    AKrylov::sort_by_bubble(arr, arr_size);
+    int low = 0;
+    int high = arr_size-1;
+    AKrylov::quickSort(arr, low, high);
     IOArray::print_array("Отсортированный массив: ", arr, arr_size);
 }
 
--- Solution/SmartSort/sortings/akrylov_sortings.cpp
+++ Solution/SmartSort/sortings/akrylov_sortings.cpp
@@ -1,4 +1,6 @@
 #include "akrylov_sortings.hpp"
+void swap(int &a, int &b);
+int partition(int arr[], int low, int high);
 
 void AKrylov::sort_by_bubble(int arr[], const int n) {
     for (int i = 0; i < n - 1; i++) {
@@ -10,4 +12,29 @@ void AKrylov::sort_by_bubble(int arr[], const int n) {
             }
         }
     }
+}
+void AKrylov::quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+        AKrylov::quickSort(arr, low, pivotIndex - 1);
+        AKrylov::quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
 }
