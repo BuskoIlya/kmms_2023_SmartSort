d0eab0179e7d74f4c1b6c04ad51a4a5f3dbbbf27
Andrei Sulimov <avega2003@yandex.ru>
2023-11-02 22:03:09 +0300

    Added GTests for Ibusko Akrylov Asulimov sorts

--- Solution/SmartSort/menu.cpp
+++ Solution/SmartSort/menu.cpp
@@ -7,11 +7,16 @@
 #include "./sortings/asulimov_sortings.hpp"
 #include "./sortings/ibusko_sortings.hpp"
 
+
 void Menu::sort_by_akrylov_bubble(const char* const comment, int arr[], const int arr_size) {
     std::cout << comment << std::endl;
     IOArray::print_array("Первоначальный массив: ", arr, arr_size);
-    AKrylov::sort_by_bubble(arr, arr_size);
-    IOArray::print_array("Отсортированный массив: ", arr, arr_size);
+    int arr_copy[arr_size];
+    for (int i = 0; i < arr_size; ++i) {
+        arr_copy[i] = arr[i];
+    }
+    AKrylov::sort_by_bubble(arr_copy, arr_size);
+    IOArray::print_array("Отсортированный массив: ", arr_copy, arr_size);
 }
 
 void Menu::sort_by_asulimov_bubble(const char* const comment, int arr[], const int arr_size) {
